9e68ed69b5693b0bdbd09f2867108da29683c7f0
ptflp <filippov.pa@s-vfu.ru>
2017-11-10 23:24:03 +0900

    http get request via Vue

---
+++ app.js
@@ -0,0 +1,25 @@
+new Vue({
+	el: '#app',
+	data: {
+		items: [],
+		totalItems: 0,
+		perPage: 9,
+		currentPage: 1
+	},
+	methods: {
+		fetchPhotos: function(page) {
+			var options = {
+				params: {
+					page: page,
+					per_page: this.perPage
+				}
+			}		
+			this.$http.get('https://s-vfu.ru/news/data.php',options).then(function(response) {
+				console.log(response);
+			},console.log)		
+		}
+	},
+	created: function () {
+		this.fetchPhotos(this.currentPage)
+	}
+})
